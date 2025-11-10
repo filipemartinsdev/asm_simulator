ce8a47c5ce41088b930e360c43749b2f56d4d9d4
filipemartinsdev <dev.filipemartins@gmail.com>
2025-11-10 18:56:58 -0300

    first commit

---
+++ src/main/asmsimulator/Kernel.java
@@ -0,0 +1,111 @@
+package main.asmsimulator;
+
+import java.util.Map;
+import java.util.HashMap;
+// import java.util.List;
+
+public final class Kernel {
+    private static Map<String, byte[]> fields = new HashMap<>();
+
+    private static final String[] REGISTERS_16BITS = { "ax", "bx", "cx", "dx" };
+    private static final String[] REGISTERS_8BITS = {
+            "al", "ah",
+            "bl", "bh",
+            "cl", "ch",
+            "dl", "dh",
+    };
+
+    static {
+        fields.put("eax", new byte[4]);
+        fields.put("ebx", new byte[4]);
+        fields.put("ecx", new byte[4]);
+        fields.put("edx", new byte[4]);
+    }
+
+    public static void call() {
+        String eaxValue = String.copyValueOf(parseBytesToChars(Kernel.fields.get("eax")));
+        switch (eaxValue) {
+            case "4":
+                Kernel.Syscall.print();
+                return;
+            default:
+                System.out.println("[ERROR] INVALID SYSCALL");
+                System.out.println(eaxValue); // TODO: FIX THIS
+                return;
+        }
+    }
+
+    public static void mov(String destin, String value) {
+
+        if (!Kernel.fields.containsKey(destin)) {
+            System.out.println("[ERROR] INVALID DESTIN FIELD");
+            return;
+        }
+
+        Kernel.fields.put(destin, parseCharsToBytes(value.toCharArray()));
+    }
+
+    public static void db(String field, String value) {
+        if (Kernel.fields.containsKey(field)) {
+            System.out.println("[ERROR] FIELD IS ALREADY DEFINED.");
+            return;
+        }
+
+        // var byteArr = new byte[value.length() * 2];
+        // int i = 0;
+        // for (char c : value.toCharArray()) {
+        //     byteArr[i] = (byte) (c & 127);
+        //     byteArr[++i] = (byte) ((c >> 8) & 127);
+        //     i++;
+        // }
+
+        Kernel.fields.put(field, Kernel.parseCharsToBytes(value.toCharArray()));
+
+    }
+
+    public static void resb(String field, int bytes) {
+        if (Kernel.fields.containsKey(field)) {
+            System.out.println("[ERROR] FIELD "+field+" IS ALREADY DEFINED");
+            return;
+        }
+
+        Kernel.fields.put(field, new byte[bytes]);
+    }
+
+    public static char[] parseBytesToChars(byte[] bytes){
+        var charArr = new char[bytes.length/2];
+
+        for(int i=0; i<charArr.length; i+=2){
+            charArr[i/2] = (char)(((bytes[i] & 0xFF ) << 8) | (bytes[i+1] & 0xFF));
+        }
+
+        return charArr;
+    }
+
+    public static byte[] parseCharsToBytes(char[] chars){
+        var byteArr = new byte[chars.length * 2];
+        int i = 0;
+
+        for (char c : chars) {
+            byteArr[i+1] = (byte) (c & 0xFF);
+            byteArr[i] = (byte) ((c >> 8) & 0xFF);
+            i+=2;
+        }
+
+        return byteArr;
+    }
+
+    private final class Syscall {
+        public static void print() {
+
+            char[] charArr = parseBytesToChars(Kernel.fields.get("ecx"));
+//            System.out.println(charArr.toString());
+        }
+
+        public static void read() {
+        }
+
+        public static void write() {
+        }
+    }
+}
---
+++ src/test/asmsimulator/Main.java
@@ -0,0 +1,4 @@
+package test.asmsimulator;
+
+public class Main {
+}
